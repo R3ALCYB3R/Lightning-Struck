c435e9c3c6edaac545eff78b2440d583df4f0879
R3ALCYB3R <rivalscompletion@gmail.com>
2026-03-22 22:13:41 -0400

    Update SwitchUIApp.swift

--- App/SwitchUIApp.swift
+++ App/SwitchUIApp.swift
@@ -2,15 +2,10 @@ import SwiftUI
 
 @main
 struct SwitchUIApp: App {
-    // This tracks the app's state (Active, Background, Inactive)
-    @Environment(\.scenePhase) var scenePhase
-
     var body: some Scene {
         WindowGroup {
-            // This starts the app with your Red animated Launch Screen
-            LaunchView()
-                // Forces Dark Mode to match the Switch console look
-                .preferredColorScheme(.dark) 
+            LaunchView() // Starts with your red animation
+                .preferredColorScheme(.dark)
         }
     }
 }
