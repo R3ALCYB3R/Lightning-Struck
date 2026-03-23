3f91c9209711bc07a069fe7dfd28252a09338a6c
R3ALCYB3R <rivalscompletion@gmail.com>
2026-03-23 19:28:31 -0400

    Delete Services directory

--- Services/GameModel.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct Game: Identifiable, Codable {
-    var id = UUID()
-    var name: String
-    var iconData: Data? // For the custom logo
-    var fileBookmark: Data? // Saves the .nsp / .xci location
-}
